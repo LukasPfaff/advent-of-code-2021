462c7c53645f7c641a8f0f94ba94def4e6b2f466
LukasPfaff <44885396+LukasPfaff@users.noreply.github.com>
2021-12-14 11:05:17 +0100

    Volatile attempt without IDE on Day 2

--- src/dayTwo/DayTwo.java
+++ src/dayTwo/DayTwo.java
@@ -1,10 +1,38 @@
 package dayTwo;
 
 public class DayTwo {
-
+	private static String inputLine;
+	private static int depth;
+	private static int horizontalPos;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner("file.txt");
+		
+		while(scanner.hasNext()){
+			inputLine = Scanner.next();
+			
+			String[] parts = inputLine.split(" ");
+			
+			switch(parts[0]){
+				case "forward":
+					horizontalPos += Integer.valueOf(parts[1]);
+					break;
+				case "up":
+					depth -= Integer.valueOf(parts[1]);
+					break;
+				case "down":
+					depth += Integer.valueOf(parts[1]);
+					break;
+				default:
+					System.out.println("Houston, wir haben ein Problem");
+					break;
+			}		
+		}			
+		scanner.close();
+		
+		System.out.println("Horizontal position: " + horizontalPos);
+		System.out.println("Depth: " + depth);
+		System.out.println("Result: " + depth * horizontalPos);
 	}
 
 }
